ad8ade2dc8b821c015c54747f67d499f5532e854
Vivek Nair <vivekmanju53@gmail.com>
2023-07-07 19:06:10 +0530

    smooth scrolling

--- js/main.js
+++ js/main.js
@@ -4,6 +4,28 @@
  	once: true
  });
 
+ $(document).ready(function() {
+	// Add smooth scrolling to all navbar links
+	$(".navbar a").on("click", function(event) {
+	  // Make sure this.hash has a value before overriding default behavior
+	  if (this.hash !== "") {
+		// Prevent default anchor click behavior
+		event.preventDefault();
+  
+		// Store hash
+		var hash = this.hash;
+  
+		// Using jQuery's animate() method to add smooth page scroll
+		$("html, body").animate(
+		  {
+			scrollTop: $(hash).offset().top
+		  },
+		  800 // Adjust scrolling speed (in milliseconds)
+		);
+	  }
+	});
+  });
+  
 jQuery(document).ready(function($) {
 
 	"use strict";
